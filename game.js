14dd75125f6a14a67fe364bfc7533d3f1d223330
Halim Njeim <michaelnjeim44@gmail.com>
2024-12-13 19:57:54 +0200

    added sound effects and prize Icons

--- game.js
+++ game.js
@@ -2,6 +2,9 @@
 let is_game_running = false; 
 let score = 0;
 let remaining_time = 10;
+let prize_sound = new Audio("sounds/harp-metal-97812.mp3");
+let winning_sound = new Audio("sounds/get-up-pierrot-atm-notification-94647.mp3");
+let losing_sound = new Audio("sounds/trumpet-fail-242645.mp3");
 
 //Declared variables
 let end;
@@ -10,6 +13,7 @@ let boundaries;
 let status_display;
 let reset;
 let countDown;
+let prize1, prize2;
 
 
 document.addEventListener("DOMContentLoaded", loadPage);
@@ -33,6 +37,7 @@ function gameOver(){
         if(score > 0)
             score = score - 1;
         displayScore("Game Over!");
+        losing_sound.play();
         clearInterval(countDown);
         is_game_running = false;
     }
@@ -41,6 +46,7 @@ function gameOver(){
 function startGame(){
     displayScore("Game has started: ");
     is_game_running = true;
+    
     for(let i = 0; i < boundaries.length; i++)
         boundaries[i].style.backgroundColor = "#eeeeee"; 
 }
@@ -51,6 +57,8 @@ function endGame(){
             boundaries[i].style.backgroundColor = "rgb(113 225 141)"; 
         score = score + 5;
         displayScore("You Won!");
+        winning_sound.play();
+        clearInterval(countDown);
         is_game_running = false;
     }
 }
@@ -71,15 +79,23 @@ function resetGame(){
     location.reload();
 }
 
+
+
 function loadPage(){
     end = document.getElementById("end");
     start = document.getElementById("start");
     boundaries = document.getElementsByClassName("boundary");
     status_display =  document.getElementById("status");
     reset = document.getElementById("reset-button");
+    prize1 = document.getElementById("star");
+    prize2 = document.getElementById("wheel");
+
 
     end.addEventListener("mouseover", endGame);
     start.addEventListener("click", startGame);
+    prize1.addEventListener("mouseover", function(){
+        prize1.hidden = true;
+    })
     for(let i = 0; i < boundaries.length; i++){
         boundaries[i].addEventListener("mouseover", gameOver);
     }
